8309f5e10b5872e3d71a5ea754f5a0f8bafee6e5
cruzcordero <cruzcordero7600@gmail.com>
2023-08-28 18:10:20 -0400

    added scrolling effect for projects to show while scrolling

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,13 @@
-import { Component, inject, OnInit } from "@angular/core";
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  inject,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 import { cruzLogosTransparent } from "src/app/shared/ImageReferences";
 import { ProjectService } from "src/app/shared/services/project.service";
 
@@ -6,8 +15,15 @@ import { ProjectService } from "src/app/shared/services/project.service";
   selector: "app-projects",
   templateUrl: "./projects.component.html",
   styleUrls: ["./projects.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, AfterViewInit {
+  @ViewChild("img") images: ElementRef;
+
+  isIntersected: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  slideInCount: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+
   backgroundColor: string = "transperent";
   imageSrc: string = cruzLogosTransparent;
   linkColor: string = "black";
@@ -20,5 +36,23 @@ export class ProjectsComponent implements OnInit {
     this.hovered = projectId;
   }
 
+  observer: IntersectionObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("intersecting");
+        this.isIntersected.next(true);
+        setTimeout(() => {
+          this.slideInCount.next(1);
+        }, 500);
+      } else {
+        this.isIntersected.next(false);
+      }
+    });
+  });
+
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.observer.observe(this.images.nativeElement);
+  }
 }
