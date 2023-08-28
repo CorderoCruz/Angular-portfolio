19f71fd75dd0a91d5cfd6a5e512572eef444b990
cruzcordero <cruzcordero7600@gmail.com>
2023-08-28 14:16:51 -0400

    removed comments from environment file

--- src/environment.ts
+++ src/environment.ts
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "AIzaSyD0rCgkoljK3ild4aK3m70B0-n2qdwZNEM",
   authDomain: "cruz-cor.firebaseapp.com",
@@ -13,5 +7,3 @@ export const firebaseConfig = {
   appId: "1:894888807210:web:f9a0a1ae9b1be3084f6c53",
   measurementId: "G-NGXKXW9VT0",
 };
-
-// Initialize Firebase
